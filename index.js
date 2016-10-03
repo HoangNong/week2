d0d1a4219a6a1d853b76b0d9c7298ad2e7793b7d
HoangNV <hoang.nong@ntq-solution.com.vn>
2016-10-03 17:11:36 +0700

    Update create and delete todo

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var http = require('http');
 var path = require('path');
 var app = express();
 var ect = require('ect');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var ectRenderer = ect({watch: true, root: __dirname + '/views', ext: '.ect'});
  
 // all environments
@@ -14,8 +16,17 @@ app.set('port', 3000);
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
 app.use('/public', express.static(path.resolve(__dirname, './public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(cookieParser());
  
 app.get('/', routes.index);
+app.post('/create', routes.create);
+app.get('/destroy/:id', routes.destroy);
+app.get('/edit/:id', routes.edit);
+app.post('/update/:id', routes.update);
  
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
